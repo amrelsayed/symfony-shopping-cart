c3e783820780779f57ee3cc412aad71f6e938602
Amr Elsayed <amr-elsayed@outlook.com>
2019-07-26 22:52:39 +0200

    removing addtocart method logic to service class

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -26,6 +26,7 @@ namespace App\Controller;
 use App\Entity\Cart;
 use App\Entity\CartProduct;
 use App\Entity\Product;
+use App\Services\CartService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -33,6 +34,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
+    private $cartService;
+
+    public function __construct(CartService $cartService)
+    {
+    	$this->cartService = $cartService;
+    }
+    
     /**
 	* @Route("/cart", name="cart")
 	*/
@@ -52,57 +60,7 @@ class CartController extends AbstractController
     */
     public function addItemToCart($product_id)
     {
-    	/*
-    	* If we have authenticated users the logic will be
-    	* 1- checking if user cart is exists (we may find cart with a condition because our database design allows user 
-    	* 	 to have multiple carts in case we may want to keep old carts data, so the current cart may have a stuus of new 
-    	* 	 or something similar) || create new one
-    	* 2- adding items to the user cart
-		*
-    	* The current senario will be
-    	* 1- I'll assume that our user holding id of 1 
-    	* 2- If the cart of the user does not exists create a new one
-    	* 3- check if the product already exists in the cart then => increase quantity
-    	* 4- add items to the cart
-    	*/
-
-    	// get user cart
-    	$user_cart = $this->getDoctrine()
-    		->getRepository(Cart::class)
-    		->findOneBy(['user_id' => 1]);
-
-    	// init doctrine manager
-    	$em = $this->getDoctrine()->getManager();
-
-    	// check if cart not found then add new cart for the user
-    	if (! $user_cart) {
-    		$user_cart = new Cart();
-    		$user_cart->setUserId(1);
-    		$em->persist($user_cart);
-    	}
-    	
-    	// get product
-    	$product = $this->getDoctrine()
-    		->getRepository(Product::class)
-    		->find($product_id);
-
-    	// check if product exists in cart_product table
-    	$cart_product = $this->getDoctrine()
-    		->getRepository(CartProduct::class)
-    		->findOneBy(['product_id' => $product_id, 'cart_id' => $user_cart->getId()]);
-
-    	if (! $cart_product) {
-    		$cart_product = new CartProduct();
-	    	$cart_product->setProduct($product);
-	    	$cart_product->setCart($user_cart);
-	    	$cart_product->setQuantity(1);
-	    	$cart_product->setPrice($product->getPrice());
-    	} else {
-    		$cart_product->setQuantity($cart_product->getQuantity() + 1);
-    	}
-    	
-    	$em->persist($cart_product);
-    	$em->flush();
+    	$this->cartService->addItemToCart($product_id);
     	
     	return $this->redirectToRoute('cart');
     }
---
+++ src/Services/CartService.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Services;
+
+use App\Entity\Cart;
+use App\Entity\CartProduct;
+use App\Entity\Product;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class CartService extends AbstractController
+{
+	/*
+    * If we have authenticated users the logic will be
+    * 1- checking if user cart is exists (we may find cart with a condition because our database design allows user 
+    *    to have multiple carts in case we may want to keep old carts data, so the current cart may have a stuus of new 
+    *    or something similar) || create new one
+    * 2- adding items to the user cart
+    *
+    * The current senario will be
+    * 1- I'll assume that our user holding id of 1 
+    * 2- If the cart of the user does not exists create a new one
+    * 3- check if the product already exists in the cart then => increase quantity
+    * 4- add items to the cart
+    */
+    public function addItemToCart($product_id)
+	{
+		// get user cart
+    	$user_cart = $this->getDoctrine()
+    		->getRepository(Cart::class)
+    		->findOneBy(['user_id' => 1]);
+
+    	// init doctrine manager
+    	$em = $this->getDoctrine()->getManager();
+
+    	// check if cart not found then add new cart for the user
+    	if (! $user_cart) {
+    		$user_cart = new Cart();
+    		$user_cart->setUserId(1);
+    		$em->persist($user_cart);
+    	}
+    	
+    	// get product
+    	$product = $this->getDoctrine()
+    		->getRepository(Product::class)
+    		->find($product_id);
+
+    	// check if product exists in cart_product table
+    	$cart_product = $this->getDoctrine()
+    		->getRepository(CartProduct::class)
+    		->findOneBy(['product_id' => $product_id, 'cart_id' => $user_cart->getId()]);
+
+    	if (! $cart_product) {
+    		$cart_product = new CartProduct();
+	    	$cart_product->setProduct($product);
+	    	$cart_product->setCart($user_cart);
+	    	$cart_product->setQuantity(1);
+	    	$cart_product->setPrice($product->getPrice());
+    	} else {
+    		$cart_product->setQuantity($cart_product->getQuantity() + 1);
+    	}
+    	
+    	$em->persist($cart_product);
+    	$em->flush();
+	}
+}
